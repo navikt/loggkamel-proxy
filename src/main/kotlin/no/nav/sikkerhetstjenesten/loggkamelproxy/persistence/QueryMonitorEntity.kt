f9849c2d79e14a91ad932ac6a1748e9bdd3671c5
Carlos Sierra <carlos.sierra@nav.no>
2026-07-29 09:43:02 +0200

    Specify that id field is transient, prepare for fallback disabling of schema validation

--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/persistence/QueryMonitorEntity.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/persistence/QueryMonitorEntity.kt
@@ -2,10 +2,9 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.persistence
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.persistence.Transient
 import org.hibernate.annotations.Immutable
 import org.hibernate.annotations.NaturalId
 import java.time.LocalDateTime
@@ -18,8 +17,7 @@ import java.util.UUID
 class QueryMonitorEntity {
 
     @Id
-    @Column(insertable = false, updatable = false)
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Transient
     var syntheticId: UUID = UUID.randomUUID()
 
     @NaturalId
