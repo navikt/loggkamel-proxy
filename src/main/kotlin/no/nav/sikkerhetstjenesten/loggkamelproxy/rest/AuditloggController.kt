e04eba03448665f1c5743f1bbac65fa54b8ad97c
Carlos Sierra <carlos.sierra@nav.no>
2026-07-29 14:23:34 +0200

    Explicitly typing controller request params

--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
@@ -3,8 +3,10 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.rest
 import no.nav.sikkerhetstjenesten.loggkamelproxy.rest.dto.AuditloggLineDTO
 import no.nav.sikkerhetstjenesten.loggkamelproxy.service.AuditloggService
 import org.slf4j.LoggerFactory
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import kotlin.time.Instant
 
@@ -16,9 +18,9 @@ class AuditloggController(val auditloggService: AuditloggService) {
 
     //TODO: manual test, confirm this takes these as part of the request body
     @PostMapping
-    fun testEndpoint(databaseName: String,
-                     logStartTime: Instant,
-                     logEndTime: Instant,) : List<AuditloggLineDTO> {
+    fun testEndpoint(@RequestParam databaseName: String,
+                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) logStartTime: Instant,
+                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) logEndTime: Instant,) : List<AuditloggLineDTO> {
 
         log.info("Request received with databaseName: $databaseName, logStartTime: $logStartTime, logEndTime: $logEndTime")
         log.info("logStarttime as instant: $logStartTime")
