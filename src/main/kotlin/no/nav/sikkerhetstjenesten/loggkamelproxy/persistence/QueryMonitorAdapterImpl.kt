aba9dc31449d310a2e8e6c960316a89cef88af7a
Carlos Sierra <carlos.sierra@nav.no>
2026-07-29 12:47:53 +0200

    Add basic logging around auditlogg controller

--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/persistence/QueryMonitorAdapterImpl.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/persistence/QueryMonitorAdapterImpl.kt
@@ -2,12 +2,15 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.persistence
 
 import no.nav.boot.conditionals.ConditionalOnFSS
 import no.nav.sikkerhetstjenesten.loggkamelproxy.rest.dto.AuditloggLineDTO
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
 @Component
 @ConditionalOnFSS
 class QueryMonitorAdapterImpl(val queryMonitorRepository: QueryMonitorRepository) : QueryMonitorAdapter {
+
+    var log = LoggerFactory.getLogger(QueryMonitorAdapterImpl::class.java)
 
     fun QueryMonitorEntity.toAuditloggLineDTO(): AuditloggLineDTO {
         return AuditloggLineDTO(
@@ -20,6 +23,8 @@ class QueryMonitorAdapterImpl(val queryMonitorRepository: QueryMonitorRepository
                                                      logEndTime: LocalDateTime,
     ): List<AuditloggLineDTO> {
         val logglinesAsDatabaseEntities = queryMonitorRepository.findAllByDatabaseNameAndMetricsTimestampBetween(databaseName, logStartTime, logEndTime)
+
+        log.info("Found ${logglinesAsDatabaseEntities.size} logglines")
 
         return logglinesAsDatabaseEntities.map { it.toAuditloggLineDTO() }
     }
--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
@@ -2,20 +2,29 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.rest
 
 import no.nav.sikkerhetstjenesten.loggkamelproxy.rest.dto.AuditloggLineDTO
 import no.nav.sikkerhetstjenesten.loggkamelproxy.service.AuditloggService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @RestController
 @RequestMapping("/v1/auditlogg")
 class AuditloggController(val auditloggService: AuditloggService) {
+
+    var log = LoggerFactory.getLogger(AuditloggController::class.java)
 
     //TODO: manual test, confirm this takes these as part of the request body
     @PostMapping
     fun testEndpoint(databaseName: String,
                      logStartTime: LocalDateTime,
                      logEndTime: LocalDateTime,) : List<AuditloggLineDTO> {
+
+        log.info("Request received with databaseName: $databaseName, logStartTime: $logStartTime, logEndTime: $logEndTime")
+        log.info("logStarttime as instant: ${logStartTime.toInstant(ZoneOffset.UTC)}")
+        log.info("logEndtime as instant: ${logEndTime.toInstant(ZoneOffset.UTC)}")
+
         return auditloggService.getLogglinesByDatabaseAndTimePeriod(databaseName, logStartTime, logEndTime)
     }
 }
