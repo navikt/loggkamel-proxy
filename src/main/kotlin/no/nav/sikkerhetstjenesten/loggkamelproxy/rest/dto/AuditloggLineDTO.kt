d851470cc0006d1b8fc1de1a0d17574d7e6ee291
Carlos Sierra <carlos.sierra@nav.no>
2026-07-30 07:56:19 +0200

    Making DTO a data class

--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/dto/AuditloggLineDTO.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/dto/AuditloggLineDTO.kt
@@ -2,4 +2,4 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.rest.dto
 
 import java.time.LocalDateTime
 
-class AuditloggLineDTO(metricsTimestamp: LocalDateTime ?= null, databaseName: String ?= null, tableName: String ?= null, authId: String ?= null, sqlQuery: String ?= null)
+data class AuditloggLineDTO(val metricsTimestamp: LocalDateTime ?= null, val databaseName: String ?= null, val tableName: String ?= null, val authId: String ?= null, val sqlQuery: String ?= null)
