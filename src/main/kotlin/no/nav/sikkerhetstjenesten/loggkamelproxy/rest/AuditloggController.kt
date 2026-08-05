3155bce5bc13c47944db57f085dc2973339c3ad0
Carlos Sierra <carlos.sierra@nav.no>
2026-08-05 10:11:06 +0200

    Update controller to accept and return json bodies

--- src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/loggkamelproxy/rest/AuditloggController.kt
@@ -2,8 +2,10 @@ package no.nav.sikkerhetstjenesten.loggkamelproxy.rest
 
 import no.nav.sikkerhetstjenesten.loggkamelproxy.rest.dto.AuditloggLineDTO
 import no.nav.sikkerhetstjenesten.loggkamelproxy.service.AuditloggService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
@@ -12,12 +14,12 @@ import java.time.LocalDateTime
 @RequestMapping("/api/v1/auditlogg")
 class AuditloggController(val auditloggService: AuditloggService) {
 
-    @PostMapping
-    fun getAuditloggsForDatabaseAndDateRange(databaseName: String,
-                                             logStartTime: LocalDateTime,
-                                             logEndTime: LocalDateTime,) : List<AuditloggLineDTO> {
+    data class AuditloggRequest(val databaseName: String, val logStartTime: LocalDateTime, val logEndTime: LocalDateTime)
 
-        return auditloggService.getLogglinesByDatabaseAndTimePeriod(databaseName, logStartTime, logEndTime)
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getAuditloggsForDatabaseAndDateRange(@RequestBody request: AuditloggRequest) : List<AuditloggLineDTO> {
+
+        return auditloggService.getLogglinesByDatabaseAndTimePeriod(request.databaseName, request.logStartTime, request.logEndTime)
     }
 
     //TODO: consider limiting to only existing in DEV, or removing entirely
